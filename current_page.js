009d38f61d38fade4587562751990a0d160b8e90
Glitch (raneemu) <none>
2020-09-01 15:28:35 +0000

    current page color update

---
+++ current_page.js
@@ -0,0 +1,23 @@
+// var images = ('.imageOutput');
+
+// (".imageUpload").change(function(event){
+//     readURL(this);
+// });
+
+// function readURL(input) {
+
+//     if (input.files && input.files[0]) {
+        
+//         $.each(input.files, function() {
+//             var reader = new FileReader();
+//             reader.onload = function (e) {           
+//                 images.append('<img src="'+ e.target.result+'" />')
+//             }
+//             reader.readAsDataURL(this);
+//         });
+        
+//     }
+// }
+
+$("a[href*='" + location.pathname + "']").addClass("current");
+/*not implemented*/
